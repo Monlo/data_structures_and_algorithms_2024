c1a152bc5dd1919550072947739708361a7a841b
Monlo <lopezmonserrat.14@gmail.com>
2024-02-18 11:15:04 +0100

    changes

--- flaskapp/flaskapp.py
+++ flaskapp/flaskapp.py
@@ -1,16 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', heading='My personal home page')
 
 
-@app.route("/hello/<city>")
-def hello(city):
-    return f"<h1>Hello, {city}!</h1>"
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
